6b9079a5f4f41e320a42959efcfca2c3904f1781
niyaz <niazhadik3@mail.ru>
2022-09-09 15:07:39 +0300

    + viewset

--- foodapp/admin.py
+++ foodapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from foodapp.models import FoodCategory, Food
+
+admin.site.register(FoodCategory)
+admin.site.register(Food)
--- foodapp/views.py
+++ foodapp/views.py
@@ -1,3 +1,24 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from foodapp.models import FoodCategory, Food
+from foodapp.serializers import FoodListSerializer
+
+
+class FoodCategoryView(viewsets.ViewSet):
+    serializer_class = FoodListSerializer
+
+    def list(self, request):
+        queryset = (
+            FoodCategory
+            .objects
+            .prefetch_related(
+                Prefetch(
+                    'food',
+                    queryset=Food.objects.prefetch_related('additional').filter(is_publish=True).order_by("internal_code")))
+            .order_by('id'))
+
+        serializer = FoodListSerializer(queryset, many=True)
+        return Response(serializer.data)
